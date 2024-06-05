b72bc1e298ffc26b0a9ec2f510a96b3a4392cb4d
augustine- <augustine.wittkower@gmail.com>
2024-06-04 19:16:17 -0700

    intro to curses

--- interactive-display.py
+++ interactive-display.py
@@ -1,1 +1,25 @@
 import curses
+
+def main(stdscr):
+    curses.curs_set(0) # Hide cursor
+    stdscr.clear() # Clear screen
+    # stdscr.refresh() # ?
+
+    display_value = 0
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 0, f"Display Value: {display_value}")
+        stdscr.addstr(2, 0, f"'W' to increase, 'S' to decrease, 'Q' to quit.")
+        # stdscr.refresh()
+
+        key = stdscr.getch()
+
+        if key == ord('w') or key == ord('W'):
+            display_value += 1
+        elif key == ord('s') or key == ord('S'):
+            display_value -= 1
+        elif key == ord('q') or key == ord('Q'):
+            break
+
+curses.wrapper(main)
